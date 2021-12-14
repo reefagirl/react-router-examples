7c4185f3b01a70dcc81e88ede88aa96e881ef6bd
reefagirl <thassim97@hotmail.com>
2021-12-14 17:07:18 +0530

    initial GA connection

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Menu from "./Components/Menu";
 import Brand from "./Components/Brand";
 import Error from "./Components/Error";
+import ReactGa from "react-ga";
 
 function App() {
+  useEffect(() => {
+    ReactGa.initialize("3101143100");
+
+    //To record page view
+    ReactGa.pageview("/");
+  }, []);
+
   return (
     <div className="App">
       <Router>
